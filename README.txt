61acdfb319197af70babdfdfaa9e7efe1a7698ea
ShoaibAk47 <shoaib1999ak@gmail.com>
2020-04-29 23:03:46 +0530

    Admin can create users and create entities on user behalf

---
+++ README.txt
@@ -0,0 +1,5 @@
+1st commit- Admin can create users and create entities on user behalf------
+
+Admin can create user, and can create entities on user behalf. A entity table is made with foreign key i.e.,user.id. A One-to-many 
+relationship is created from User to Entity.
+
---
+++ api_basic/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.postgres.fields import ArrayField
+from django.contrib.auth.models import User
+# Create your models here.
+class Entity(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    image=models.URLField()
+    tags = ArrayField(models.CharField(max_length=200), blank=True)
+    height=models.IntegerField()
+    width=models.IntegerField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
